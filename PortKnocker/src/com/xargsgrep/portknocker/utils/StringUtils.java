d0a948b6f6f7354bae654bca89dbe9d29ca53ead
Ahsan <ahsan@xargsgrep.com>
2012-05-10 01:35:55 -0400

    - get app working in 2.1 - todo: replace preference fragment with 2.1 compatible preference activity

--- PortKnocker/src/com/xargsgrep/portknocker/utils/StringUtils.java
+++ PortKnocker/src/com/xargsgrep/portknocker/utils/StringUtils.java
@@ -7,8 +7,13 @@ public class StringUtils {
 		return (stringToCheck == null) ? false : stringToCheck.contains(str);
 	}
 	
+	// String.isEmpty only available starting API 9
+	public static boolean isEmpty(String str) {
+		return (str == null || str.length() == 0);
+	}
+	
 	public static boolean isBlank(String str) {
-		return (str == null || str.trim().isEmpty());
+		return (str == null || isEmpty(str.trim()));
 	}
 	
 	public static boolean isNotBlank(String str) {
