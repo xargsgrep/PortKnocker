4df56c0c49df648686568726d2eb35f7f2dfd906
Ahsan Rabbani <ahsan@xargsgrep.com>
2012-05-13 20:01:51 -0400

    added WTFPL license

--- PortKnocker/src/com/xargsgrep/portknocker/utils/SocketUtils.java
+++ PortKnocker/src/com/xargsgrep/portknocker/utils/SocketUtils.java
@@ -1,3 +1,13 @@
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ *
+ * Ahsan Rabbani <ahsan@xargsgrep.com>
+ *
+ */
 package com.xargsgrep.portknocker.utils;
 
 import java.io.IOException;
